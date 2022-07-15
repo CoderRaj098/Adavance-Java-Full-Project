4cc803469a47daa26065886eba909edea09ad1a6
raj singh <rajsingh191998@gmail.com>
2022-07-15 12:47:25 +0530

    assignment created

---
+++ async assignment/raj.js
@@ -0,0 +1,12 @@
+var input = `6
+1 2 2 3 4 5
+6
+1 2 3 4 5 5`;
+
+input = input.split("\n");
+var arr1 = input[1].split(" ").map(Number);
+var arr2 = input[2].split(" ").map(Number);
+function jimObj(arr){
+    var obj = {};
+    
+}
